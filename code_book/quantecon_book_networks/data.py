1fed34aa47a6dc1261b9f3d3fcae6f33aa613b07
dawkinsmw <mark.dawkins94@gmail.com>
2022-03-06 12:07:10 +1100

    initial package commit

---
+++ code_book/quantecon_book_networks/data.py
@@ -0,0 +1,128 @@
+import numpy as np
+import pandas as pd
+import networkx as nx
+import json
+
+## Utilities
+def read_Z(data_file='data/adjacency_matrix_31-12-2019.csv', t=10):
+    """
+    Build the Z matrix from the use table.
+    
+    * Z[i, j] = sales from sector i to sector j
+    
+    """
+    
+    df1 = pd.read_csv(data_file)
+    df1 = df1.set_index("country")
+
+    df2 = df1.replace(np.nan, 0)          # replace nan with 0
+
+    df3 = df2.replace("...", 0)          # replace ... with 0
+
+    countries = list(df3.index)
+    countries = np.array(countries)
+    countries = np.where(countries == 'CH', 'SW', countries)
+
+    Z = np.asarray(df3.values.tolist(), dtype=np.float64)
+    Z_visual = np.where(Z < t, 0, Z)
+    
+    output = {'Z':Z,'Z_visual':Z_visual, 'countries':countries}
+    return output
+
+
+## Chapter data
+
+def introduction():
+    ch_data = {}
+
+    ## Crude oil
+    fl = "data/crude_oil_sitcr2_3330_yr2019/data.csv"
+    crude_oil = pd.read_csv(fl, dtype={'product_id': str})
+
+    exporters = crude_oil.groupby(by=["location_code"]).sum().sort_values("export_value", ascending=False)[:10].index
+
+    importers = crude_oil.groupby(by=["partner_code"]).sum().sort_values("export_value", ascending=False)[:21].index
+    importers = set(importers.drop("ANS"))
+
+    # Aggregate Data for Rest of the World
+    row_concord = {}
+    other_importers = set(crude_oil.partner_code.unique()).difference(importers)
+    for cntry in other_importers:
+        row_concord[cntry] = "ROW"
+    importers.add("ROW")
+    importers = pd.Index(importers, name='partner_code')
+
+    # Aggregate Partner Locations
+    crude_oil.partner_code = crude_oil.partner_code.replace(to_replace=row_concord)
+
+    chart_data = crude_oil.groupby(by=["location_code", "partner_code"]).sum().reset_index()
+
+    # country data
+    cdata = pd.read_csv("data/crude_oil_sitcr2_3330_yr2019/regions-iso3c.csv")
+    country_names = cdata[["alpha-3","name"]].set_index("alpha-3").to_dict()['name']
+    country_names["ROW"] = "Rest of World"
+    country_names['TWN'] = "Taiwan"
+    country_names['GBR'] = "United Kingdom"
+
+    DG_crude = nx.DiGraph()
+    for idx,row in chart_data.iterrows():       
+        if row.location_code not in exporters:
+            continue
+        if row.partner_code not in importers:
+            continue
+        DG_crude.add_weighted_edges_from([(country_names[row.location_code], country_names[row.partner_code], row.export_value)])
+
+    ch_data["crude_oil"] = DG_crude
+
+    ## aircraft_network_2019
+    DATA_DIR = "data/commercial-aircraft-sitcr2-7924-yr2019"
+    
+    ch_data["aircraft_network_2019"] = nx.read_gexf(f"{DATA_DIR}/sitcr2-7924-aircraft-network-2019.gexf")
+
+    f = open(f"{DATA_DIR}/sitcr2-7924-aircraft-network-2019-layout.json", "r")
+    data = json.loads(f.read())
+    pos = {}
+    for nd in data['nodes']:
+        pos[nd['id']] = np.array([nd['x'], nd['y']])
+    ch_data["aircraft_network_2019_pos"] = pos
+    
+    ## forbes-global2000
+    dfff = pd.read_csv('data/csv_files/forbes-global2000.csv')
+    dfff = dfff[['Country', 'Sales', 'Profits', 'Assets', 'Market Value']]
+    dfff = dfff.sort_values('Market Value', ascending=False)
+    ch_data["forbes_global_2000"] = dfff
+    
+    ## adjacency_matrix_2019
+    ch_data["adjacency_matrix_2019"] = read_Z(data_file='data/csv_files/adjacency_matrix_31-12-2019.csv', t=0)
+
+    return ch_data
+
+
+def production():
+    ch_data = {}
+
+    return ch_data 
+
+
+def optimal_flows():
+    ch_data = {}
+
+    return ch_data
+
+
+def markov_chains_and_networks():
+    ch_data = {}
+
+    return ch_data
+
+
+def nonlinear_interactions():
+    ch_data = {}
+
+    return ch_data
+
+
+def appendix():
+    ch_data = {}
+
+    return ch_data
---
+++ code_book/quantecon_book_networks/input_output.py
@@ -0,0 +1,162 @@
+from initialize import *
+import pandas as pd
+import matplotlib.cm as cm           # import colormap stuff!
+import networkx as nx
+import matplotlib.colors as plc
+from scipy.stats import beta
+import statsmodels.api as sm
+from interpolation import interp
+import itertools
+
+def spec_rad(M):
+    """
+    Compute the spectral radius of M.
+    """
+    return np.max(np.abs(np.linalg.eigvals(M)))
+
+
+def build_unweighted_matrix(Z, tol=1e-5):
+    """
+    return a unweighted adjacency matrix
+    """
+    return 1*(Z>tol)
+
+
+def katz_centrality(A, b=1, authority=False):
+    """
+    Computes the Katz centrality of A, defined as the x solving
+
+    x = 1 + b A x    (1 = vector of ones)
+
+    Assumes that A is square.
+
+    If authority=True, then A is replaced by its transpose.
+    """
+    n = len(A)
+    I = np.identity(n)
+    C = I - b * A.T if authority else I - b * A
+    return np.linalg.solve(C, np.ones(n))
+
+
+def eigenvector_centrality(A, k=40, authority=False):
+    """
+    Computes the dominant eigenvector of A. Assumes A is 
+    primitive and uses the power method.  
+    
+    """
+    A_temp = A.T if authority else A
+    n = len(A_temp)
+    r = spec_rad(A_temp)
+    e = r**(-k) * (np.linalg.matrix_power(A_temp, k) @ np.ones(n))
+    return e / np.sum(e)
+
+
+def build_coefficient_matrices(Z, X):
+    """
+    Build coefficient matrices A and F from Z and X via 
+    
+        A[i, j] = Z[i, j] / X[j] 
+        F[i, j] = Z[i, j] / X[i]
+    
+    """
+    A, F = np.empty_like(Z), np.empty_like(Z)
+    n = A.shape[0]
+    for i in range(n):
+        for j in range(n):
+            A[i, j] = Z[i, j] / X[j]
+            F[i, j] = Z[i, j] / X[i]
+
+    return A, F
+
+
+def to_zero_one(x):
+    "Map vector x to the zero one interval."
+    x = np.array(x)
+    x_min, x_max = x.min(), x.max()
+    return (x - x_min)/(x_max - x_min)
+
+
+def to_zero_one_beta(x, 
+                qrange=[0.25, 0.75], 
+                beta_para=[0.5, 0.5]):
+    
+    """
+    Nonlinearly map vector x to the zero one interval with beta distribution.
+    https://en.wikipedia.org/wiki/Beta_distribution
+    """
+    x = np.array(x)
+    x_min, x_max = x.min(), x.max()
+    if beta_para != None:
+        a, b = beta_para
+        return beta.cdf((x - x_min) /(x_max - x_min), a, b)
+    else:
+        q1, q2 = qrange
+        return (x - x_min) * (q2 - q1) /(x_max - x_min) + q1
+
+
+def adjacency_matrix_to_graph(A, 
+               codes,
+               tol=0.0):  # clip entries below tol
+    """
+    Build a networkx graph object given an adjacency matrix
+    """
+    G = nx.DiGraph()
+    N = len(A)
+
+    # Add nodes
+    for i, code in enumerate(codes):
+        G.add_node(code, name=code)
+
+    # Add the edges
+    for i in range(N):
+        for j in range(N):
+            a = A[i, j]
+            if a > tol:
+                G.add_edge(codes[i], codes[j], weight=a)
+
+    return G
+
+## Nodes
+def node_total_exports(G):
+    node_exports = []
+    for node1 in G.nodes():
+        total_export = 0
+        for node2 in G[node1]:
+            total_export += G[node1][node2]['weight']
+        node_exports.append(total_export)
+    return node_exports
+
+def node_total_imports(G):
+    node_imports = []
+    for node1 in G.nodes():
+        total_import = 0
+        for node2 in G[node1]:
+            total_import += G[node2][node1]['weight']
+        node_imports.append(total_import)
+    return node_imports
+
+## Edges
+def edge_weights(G):
+    edge_weights = [G[u][v]['weight'] for u,v in G.edges()]
+    return edge_weights
+
+## Plotting prep
+def normalise_weights(weights,scalar=1):
+    max_value = np.max(weights)
+    return [scalar * (weight / max_value) for weight in weights]
+
+def colorise_weights(weights,zero_one_func=to_zero_one_beta,color_palette=cm.plasma):
+    return color_palette(zero_one_func(weights))
+
+## Random graphs
+def erdos_renyi_graph(n=100, p=0.5, seed=1234):
+    "Returns an Erdős-Rényi random graph."
+    
+    np.random.seed(seed)
+    edges = itertools.combinations(range(n), 2)
+    G = nx.Graph()
+    
+    for e in edges:
+        if np.random.rand() < p:
+            G.add_edge(*e)
+    return G
